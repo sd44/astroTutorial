05e9e6711c8ffc5ceba1e22b14084b4cba34818f
sd44 <sd44sd44@yeah.net>
2025-05-28 00:24:24 +0800

    finished

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 import type { APIContext } from 'astro';
 
 export async function GET(context: APIContext) {
+  const posts = await getCollection("blog");
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site?.toString() || '',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>zh-hans</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
   });
 }
