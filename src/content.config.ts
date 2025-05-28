ea38b47dd490c65977294da8b1434b4c4f990826
sd44 <sd44sd44@yeah.net>
2025-05-28 12:01:28 +0800

    aaa

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,27 @@
 // 导入 glob 加载器（loader）
-import { glob } from "astro/loaders";
+import { glob } from 'astro/loaders';
 // 从 `astro:content` 导入工具函数
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content';
+
+const blogSchema :z.ZodSchema = z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  })
+
 // 为每个集合定义一个 `loader` 和 `schema`
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
-    })
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/blog' }),
+  schema: blogSchema,
 });
 // 导出一个单独的 `collections` 对象用以注册你的集合（们）
 export const collections = { blog };
+
+// 用 z.infer 推导类型，避免手写接口
+export type PostFrontmatter = z.infer<typeof blogSchema>;
